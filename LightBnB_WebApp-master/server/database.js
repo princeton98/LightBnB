a04773df6fcd59048bf80c87dfb7cf51281c371b
Princeton Arulnesan <princeton.arul@gmail.com>
2020-05-28 19:27:44 +0000

    updated getAllReservations function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -82,7 +82,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  //return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, reservations.*, AVG(property_reviews.rating) as average_rating
+FROM property_reviews
+JOIN users ON guest_id = users.id
+JOIN properties ON property_id = properties.id
+JOIN reservations ON reservation_id = reservations.id
+WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date LIMIT $2`, [guest_id, limit])
+.then(res => res.rows)
+
 }
 exports.getAllReservations = getAllReservations;
 
